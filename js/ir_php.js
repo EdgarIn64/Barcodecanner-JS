da2964b6c5fe51a6ea86fd9a621b1ff675ca2297
Edgar Iñaki Coria Perez <79272954+EdgarIn64@users.noreply.github.com>
2023-10-16 17:29:58 -0600

    Update direction ir_php.js

--- js/ir_php.js
+++ js/ir_php.js
@@ -1,41 +1,39 @@
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const formulario = urlParams.get('formulario')
+const host = urlParams.get('host')
+
 function entrarFormulario() {
 	const codigo = document.getElementById('codigo_barra').value
-	const queryString = window.location.search
-	const urlParams = new URLSearchParams(queryString)
-	const formulario = urlParams.get('formulario')
 	let dir = queryString + '&codigo=' + codigo
 
 	if (formulario == null)
-		window.open('http://10.100.16.107/escaner/ord_prod/escaner.php' + dir, '_self')
+		window.open('http://' + host + '/escaner/ord_prod/escaner.php' + dir, '_self')
 	else {
 		dir = queryString + '&orden=' + document.getElementById('codigo_barra').value
-		window.open('http://10.100.16.107/escaner/ord_prod/' + formulario + '.php' + dir, '_self')
+		window.open('http://' + host + '/escaner/ord_prod/' + formulario + '.php' + dir, '_self')
 	}
 
 }
 
 function cancelar() {
-	const dir = window.location.search
-	const urlParams = new URLSearchParams(dir)
-	const formulario = urlParams.get('formulario')
-
 	if (formulario == null)
-		window.open('http://10.100.16.107/escaner/ord_prod/escaner.php' + dir, '_self')
+		window.open('http://' + host + '/escaner/ord_prod/escaner.php' + queryString, '_self')
 	else
-		window.open('http://10.100.16.107/escaner/ord_prod/' + formulario +'.php' + dir, '_self')
+		window.open('http://' + host + '/escaner/ord_prod/' + formulario +'.php' + queryString, '_self')
 }
+
 
 function entrarInventario() {
 	const codigo = document.getElementById('codigo_barra').value
-	const searh = window.location.search
-	let dir = searh + '&codigo=' + codigo
+	let dir = queryString + '&codigo=' + codigo
 
-	window.open('http://10.100.16.107/escaner/invF/escaner.php' + dir, '_self')
+	window.open('http://' + host + '/escaner/invF/escaner.php' + dir, '_self')
 }
 
 function cancelarInventario() {
 	const dir = window.location.search
 
-	window.open('http://10.100.16.107/escaner/invF/escaner.php' + dir, '_self')
+	window.open('http://' + host + '/escaner/invF/escaner.php' + dir, '_self')
 }
 
